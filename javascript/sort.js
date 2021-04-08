8dab40b0c6679aebdd2f131634e48e64a2d63d93
taiyo8911 <68884687+taiyo8911@users.noreply.github.com>
2021-04-08 21:45:46 +0900

    Add files via upload

--- javascript/sort.js
+++ javascript/sort.js
@@ -1,56 +1,63 @@
-function ReverseSort() {
-    let lineData = [];
-    let element;
+// function ReverseSort() {
+//     // 並び替えるため、各行のデータを配列に保管する。
+//     let lineData = [];
 
-    // テーブルの各行の内容を取得して配列lineDataに入れる ・・・ (1)
-    let i = 0;
-    while (element = document.getElementById("tr" + (i + 1))) {
-        lineData[i] = element.innerHTML;
-        i++;
-    }
+//     for (i = 0; i < maxNum; i++) {
+//         let element = document.getElementById("tr" + (i + 1));
+//         lineData[i] = element.innerHTML;
+//     }
 
-    // 逆順に並び替える ・・・ (2)
-    lineData.sort().reverse();
+//     // lineDataを逆順に並び替える
+//     lineData.sort().reverse();
 
-    // 並び替えた結果を各行へ書き出す ・・・ (3)
-    for (i = 0; i < lineData.length; i++) {
-        document.getElementById("tr" + (i + 1)).innerHTML = lineData[i];
-    }
-
-}
+//     // 並び替えた結果を、各行へ書き出す
+//     for (i = 0; i < lineData.length; i++) {
+//         document.getElementById("tr" + (i + 1)).innerHTML = lineData[i];
+//     }
+// }
 
 
+
+
+// 並び替え用に、ランキングをつける関数を定義
+const ranking = (arr, num) => [1, 2].indexOf(num) < 0 ? null : arr.map((x, y, z) => z.filter(w => (num == 2) ? w < x : w > x).length + 1);
+
+
+// 高さで並び替える
 function HeightSort() {
-
-    let lineData = [];
+    let heightTable = [];
     let element;
-
-    let heightData = [];
     let height;
+    let rank = [];
 
-    // 各行の内容を取得して、1件ずつ配列lineDataに入れる ・・・ (1)
-    let i = 0;
-    while (element = document.getElementById("tr" + (i + 1))) {
-        lineData[i] = element.innerHTML;
-        i++;
+    // たかさを比較するため、全ポケモンのたかさを配列に入れる
+    for (i = 0; i < maxNum; i++) {
+        element = document.getElementById("height" + (i + 1));
+        height = element.textContent;
+        heightTable[i] = height;
     }
 
-    // 全ポケモンのたかさを配列に入れる
-    let a = 0;
-    while (height = document.getElementById("height" + (a + 1))) {
-        height = height.textContent;
-        heightData.push(height);
-        a++;
-    }
-
-    // 降順でランキングをつける
-    const ranking=(arr,num)=>[1,2].indexOf(num)<0?null:arr.map((x,y,z)=>z.filter(w=>(num==2)?w<x:w>x).length+1);
-    let b = heightData.map(Number);
-
-    console.log(ranking(b,1));
-
-    console.log(heightData.map(Number));
-
+    // ランキング関数を実行（引数：1は降順、2は昇順）
+    rank = ranking(heightTable, 1)
     
 
+    // 並び替えるため、各行のデータを配列に保管する。
+    let lineData = [];
+
+    for (i = 0; i < maxNum; i++) {
+        let element = document.getElementById("tr" + (i + 1));
+        lineData[i] = element.innerHTML;
+    }
+
+    // 配列のインデックスを取得する
+    var index =[];
+    for (i = 1; i < rank.length+1; i++) {
+        index.push(rank.indexOf(i));
+    }
+
+
+    // 並び替え結果を書き出す
+    for (i = 0; i < lineData.length; i++) {
+        document.getElementById("tr" + (i + 1)).innerHTML = lineData[index[i]];
+    }
 }
